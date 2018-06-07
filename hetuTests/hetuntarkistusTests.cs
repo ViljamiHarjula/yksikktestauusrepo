930732d999e3b8aef7712e7dff66c16134b1c491
Viljami Harjula <viljami.harjula@hotmail.com>
2018-06-07 16:37:20 +0300

    Add project files.

---
+++ hetuTests/hetuntarkistusTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using hetu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hetu.Tests
+{
+    [TestClass()]
+    public class hetuntarkistusTests
+    {
+        [TestMethod()]
+        public void HeTuOkTest()
+        {
+            hetuntarkistus t = new hetuntarkistus();
+            string hetu = "121288-1233";
+            bool expected = true;
+            bool actual = t.HeTuOk(hetu);
+            Assert.AreEqual(expected, actual, "Joku pielessä, pitäisi olla ok hetu");
+
+            Assert.Fail();
+        }
+    }
+}
